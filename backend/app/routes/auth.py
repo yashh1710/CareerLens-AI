508583a6b9af8fbc104e7bf49613b1cc4e6a5e8d
yashhh1710 <yashsalgaonkar1710@gmail.com>
2026-06-09 15:10:39 +0530

    Implement JWT login authentication

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -10,6 +10,10 @@ from app.schemas.user import UserCreate
 
 from app.utils.security import hash_password
 
+from app.schemas.user import UserLogin
+from app.utils.security import verify_password
+from app.utils.security import create_access_token
+
 router = APIRouter(
     prefix="/auth",
     tags=["Authentication"]
@@ -45,4 +49,41 @@ def register_user(
 
     return {
         "message": "User registered successfully"
+    }
+@router.post("/login")
+def login_user(
+    user: UserLogin,
+    db: Session = Depends(get_db)
+):
+
+    db_user = db.query(User).filter(
+        User.email == user.email
+    ).first()
+
+    if not db_user:
+
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid credentials"
+        )
+
+    if not verify_password(
+        user.password,
+        db_user.password
+    ):
+
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid credentials"
+        )
+
+    access_token = create_access_token(
+        {
+            "sub": db_user.email
+        }
+    )
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
     }
--- backend/app/utils/security.py
+++ backend/app/utils/security.py
@@ -1,22 +1,39 @@
+from datetime import datetime, timedelta
+from jose import jwt
 from passlib.context import CryptContext
+
+SECRET_KEY = "careerlens_secret_key"
+ALGORITHM = "HS256"
 
 pwd_context = CryptContext(
     schemes=["bcrypt"],
     deprecated="auto"
 )
 
-
-def hash_password(password: str):
-
+def hash_password(password):
     return pwd_context.hash(password)
-
 
 def verify_password(
     plain_password,
     hashed_password
 ):
-
     return pwd_context.verify(
         plain_password,
         hashed_password
+    )
+
+def create_access_token(data: dict):
+
+    to_encode = data.copy()
+
+    expire = datetime.utcnow() + timedelta(hours=24)
+
+    to_encode.update(
+        {"exp": expire}
+    )
+
+    return jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
     )
